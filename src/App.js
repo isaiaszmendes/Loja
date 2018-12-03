51ca3c5a5a5f2c913730ead84555bab0744e5de1
Isaias Mendes <isaiaszmendes@gmail.com>
2018-12-03 05:46:56 -0200

    app funcionando pesquisa, consumindo api realtime

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import firebase from 'firebase'
+import config  from './config'
+import axios from 'axios'
 import './App.css'
 // Components
 import Header from './components/Header'
 import ListProducts from './components/ListProducts'
+import ListSearch from './components/ListSearch'
+import Product from './components/Product'
+
+function searchInObject(req){
+    return function(x){
+        return console.log(x, req);
+    }
+}
 
 class App extends Component {
 
@@ -12,32 +23,77 @@ class App extends Component {
         this.state = {
             resultSearch: "",
             pesquisando: false,
+            produtos: {},
         }
 
         this.resetSearch    = this.resetSearch.bind(this)
         this.resultSearch      = this.resultSearch.bind(this)
 
+        // config.syncState('produtos',{
+        //     context: this,
+        //     state: 'produtos',
+        //     asArray: false
+        // })
+
     }
+    componentDidMount(){
+        this.resultSearch(this.state.resultSearch)
+    }
+   
 
     resultSearch = (params) =>{
-        this.setState({pesquisando: true})
-        this.setState({resultSearch: params})
-
+        if(params !== ""){
+            this.setState({pesquisando: true})
+            this.setState({resultSearch: params})
+            const url = `https://loja-mmartan.firebaseio.com/produtos.json?orderBy="title"&equalTo="${params}"`
+            axios.get(url)
+                .then(dados => {
+                    this.setState({
+                        produtos: dados.data,
+                        pesquisando: false
+                    })
+                })
+                .catch(err => {
+                    console.log('Deu ruim',err);
+                })
+                
+        }else{
+            this.setState({pesquisando: true})
+            const url = `https://loja-mmartan.firebaseio.com/produtos.json?orderBy="title"`
+            axios.get(url)
+                .then(dados => {
+                    this.setState({
+                        produtos: dados.data,
+                        pesquisando: false
+                    })
+                })
+                .catch(err => {
+                    console.log('Deu ruim',err);
+                })
+        }
        
     } 
 
-    resetSearch = () =>{        
+    resetSearch = () =>{   
+      
         this.setState({
             resultSearch: '',
             pesquisando: false
         })
-
-
+        
     }
 
     render() {
-        console.log(this.state, 'antes do render')
-        if (true) {
+        console.log(this.state.pesquisando, 'antes do render')
+        // if (this.state.pesquisando) {
+            if (this.state.pesquisando) {
+                return (
+                    <div>
+                        <p>Carregando .... </p>
+                        {/* {} */}
+                    </div>
+                )
+            }
             return (
                 <div className="App">
                     <Header 
@@ -45,28 +101,45 @@ class App extends Component {
                         resetSearch={this.resetSearch}
                         titulo={this.state.resultSearch}
                         />
+                    {/* <p>{console.log(JSON.stringify(this.state.produtos))}</p> */}
                     <div className='App-container'>
-                        <ListProducts 
-                            pesquisando={this.state.pesquisando}
-                            result={this.state.resultSearch}
-                        />
+                        {/* <ListSearch  />  */}
+                        <h5>Total: {Object.keys(this.state.produtos).length}</h5>
+                        {
+                            
+                           Object.keys(this.state.produtos)
+                            .map(key => {
+                                // console.log(key);
+                                return  <Product key={key}
+                                        title={this.state.produtos[key].title}
+                                        description={this.state.produtos[key].description}
+                                        nameImg={this.state.produtos[key].img.name}
+                                        url={this.state.produtos[key].img.url}
+                                        price={this.state.produtos[key].price}
+                                    />
+                            })
+                        }
+                        
                     </div>
                 </div>          
             )
-        }else{
-            return (
-                <div className="App">
-                    <Header 
-                        resultSearch={this.resultSearch}
-                        resetSearch={this.resetSearch}
-                        titulo={this.state.resultSearch}
-                    />
-                    <div className='App-container'>
-                        <ListProducts  />
-                    </div>
-                </div>          
-            )
-        }
+        // }else{
+        //     return (
+        //         <div className="App">
+        //             <Header 
+        //                 resultSearch={this.resultSearch}
+        //                 resetSearch={this.resetSearch}
+        //                 titulo={this.state.resultSearch}
+        //             />
+        //             <div className='App-container'>
+        //                 <ListProducts 
+        //                     pesquisando={this.state.pesquisando}
+        //                     result={this.state.resultSearch}
+        //                 />
+        //             </div>
+        //         </div>          
+        //     )
+        // }
 
     }
 }
--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -1,4 +1,4 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import './ListProducts.css'
 import Product from './Product'
 import config  from '../config'
@@ -9,54 +9,35 @@ class ListProducts extends Component {
         super(props)
 
         this.state = {
-            result: props.result,
-            pesquisando: props.pesquisando || false,
             produtos: {},
         }
-        
+
         config.syncState('produtos',{
             context: this,
             state: 'produtos',
             asArray: false
         })
-        console.log(props);
-
-        console.log('Status Pesquisando', this.state.pesquisando)
-
     }
 
-    render() {
-        if (this.state.pesquisando) {
-            return (
-                <Product 
-                    title='adsasd'
-                    url='http://placekitten.com/g/150/300'
-                    />
-            )
-        }else{
-            return (
-                <div>
-                    <h5>Todos os Produtos: {Object.keys(this.state.produtos).length}</h5>
-                    <p>Não pesquinsando, exibir a lista de onjetos do banco
-                       
-                    </p>
-                    {
-                        Object.keys(this.state.produtos).map(key => {
-                           return  <Product key={key}
-                                    title={this.state.produtos[key].title}
-                                    description={this.state.produtos[key].description}
-                                    nameImg={this.state.produtos[key].img.name}
-                                    url='http://placekitten.com/g/250/200'
-                                    price={this.state.produtos[key].price}
-                                />
-                        })
-                    }
-                </div>
-            )
-        }
-        
+    render(){
+        return (
+            <div>
+                <h5>Todos os Produtos: {Object.keys(this.state.produtos).length}</h5>
+                <p>Não pesquinsando, exibir a lista de objetos do banco </p>
+                {
+                    Object.keys(this.state.produtos).map(key => {
+                        return  <Product key={key}
+                                title={this.state.produtos[key].title}
+                                description={this.state.produtos[key].description}
+                                nameImg={this.state.produtos[key].img.name}
+                                url='http://placekitten.com/g/250/200'
+                                price={this.state.produtos[key].price}
+                            />
+                    })
+                }
+            </div>
+        )                
     }
-
 }
 
 export default ListProducts 
