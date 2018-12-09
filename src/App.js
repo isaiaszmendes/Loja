bbeaa811c7e3fdfe9dc91bc6399a7bc28330c50f
Isaias Mendes <isaiaszmendes@gmail.com>
2018-12-09 21:27:47 -0200

    Add paginação e melhorias na pesquisa, por title or description

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import Pagination from "react-js-pagination";
+import React, { Component } from 'react'
+import Pagination from "react-js-pagination"
 import axios from 'axios'
 import './App.css'
 // Components
 import Header from './components/Header'
 import Product from './components/Product'
 import Loading from './components/Loading'
-
 
 class App extends Component {
 
@@ -17,11 +16,10 @@ class App extends Component {
             resultSearch: "",
             pesquisando: false,
             produtos: {},
+            pesquisaFiltro: {},
             activePage: 1,
-            totalItems: 4,
-            countPages: 0,
+            totalItems: 10,
             countItems: 0
-
         }
 
         this.resetSearch        = this.resetSearch.bind(this)
@@ -30,88 +28,115 @@ class App extends Component {
 
     }
 
+    componentDidMount(){
+        this.carregaDadosIniciais()
+    }
+    
+    // Filtra de acordo com a pesquisa
+    MySearch = (obj, pesq) => {    
+        
+        const listaDeProdutos = []
+        pesq = pesq.toLowerCase()
+        for(let i=0; i < obj.length; i++){
+            const title  = obj[i].title.toLowerCase().indexOf(pesq) !== -1
+            const description  = obj[i].description.toLowerCase().indexOf(pesq) !== -1
+            if(title || description){
+                listaDeProdutos.push(obj[i])
+            }
+        }
+
+        const primeiraL = pesq.charAt(0).toUpperCase()
+        const restoPalavra = pesq.substr(1,(pesq.length - 1))
+
+        this.setState({
+            pesquisaFiltro: listaDeProdutos,
+            resultSearch: `${primeiraL}${restoPalavra}`,
+             
+        })
+        return listaDeProdutos
+    }
+
     listItems = (items, pageActual, limitItems) => {
         let result = [];
         let totalPage = Math.ceil( items.length / limitItems );
         let count = ( pageActual * limitItems ) - limitItems;
         let delimiter = count + limitItems;
-
-        // this.setState({
-        //     countPages: totalPage
-        // })
-        // console.log(totalPage);
         
         if(pageActual <= totalPage){
-            for(let i=count; i<delimiter; i++){
-                if(items[i] != null){
-                    result.push(items[i]);
+            for(let i=count; i < delimiter; i++){
+                if(items[i] != null){            
+                    result.push(items[i]);                  
                 }
                 count++;
             }
         }
-    
-        // console.log(result)
+
         return result
     }
 
     handlePageChange(pageNumber) {
-        console.log(`active page is ${pageNumber}`);
-        this.setState({activePage: pageNumber});
-        this.getData(this.state.resultSearch)
-    }
-      
-    componentDidMount(){
-        this.resultSearch(this.state.resultSearch)
-    }
+        this.setState({activePage: pageNumber})
+    }      
 
     loadingTime = (seconds) =>{
         setTimeout(()=>{
-            this.setState({
+            this.setState({    
                 pesquisando: false
             })
         }, seconds)
     }
    
-    getData = (params = null) => {
+    getData = () => {
         this.setState({pesquisando: true})
-
-        if (params) {
-            this.setState({resultSearch: params})
-        }else{
-            this.setState({resultSearch: ''})
-        }
-
-        // Verifica se existe parametros para buscar, se não houver, lista tudo
-        const query = params ? `&equalTo="${params}"` : ''
-        const url = `https://loja-mmartan.firebaseio.com/produtos.json?orderBy="title"${query}`
+        const url = `https://loja-mmartan.firebaseio.com/produtos.json`
 
         axios.get(url)
             .then(dados => dados.data)
             .then((produtos) => {
-                console.log(produtos);
+                var id = 0
+                for (const key in produtos) {
+                    produtos[key].id = id
+                    id++
+                }
+
                 this.setState({
                     produtos: produtos,
-                    countItems: produtos.length
-                    
+                    pesquisaFiltro: produtos,
+                    countItems: produtos.length                    
                 })
-
             })
             .catch(err => {
                 console.log('Deu ruim',err);
             }) 
 
         // Chama a função de animação Loading  
-        this.loadingTime(4000)        
+        this.loadingTime(1000)        
     }
 
-
-
+    // carrega os dados quando a aplicação inicia pela primeira vez
+    carregaDadosIniciais = () =>{
+        this.getData()     
+    } 
+    
+    // Filtra os produtos já carregados melhorando o desenopenho da aplicação
     resultSearch = (params) =>{
-        this.getData(params)       
+        const filtro = this.MySearch(this.state.produtos, params)   
+
+        this.setState({
+            pesquisaFiltro: filtro,
+            countItems: filtro.length,
+            activePage: 1     
+        })    
     } 
 
+    // Reinicia os dados que já estão carregados desde o inicio da aplicação
     resetSearch = () =>{
-        this.getData()               
+        this.setState({
+            pesquisaFiltro: this.state.produtos,
+            countItems: this.state.produtos.length,
+            resultSearch: "",
+            activePage: 1
+        })             
     }
 
     render() {
@@ -132,14 +157,15 @@ class App extends Component {
                     </div>
                     <div className='App-container'>                                          
                         {
-                            Object.keys(this.listItems(this.state.produtos, this.state.activePage, 4))
-                            .map((key) => {
-                                return  <Product key={key}
-                                        title={this.state.produtos[key].title}
-                                        description={this.state.produtos[key].description}
-                                        nameImg={this.state.produtos[key].img.name}
-                                        url={this.state.produtos[key].img.url}
-                                        price={this.state.produtos[key].price}
+                            Object.entries(this.listItems(this.state.pesquisaFiltro, this.state.activePage, this.state.totalItems))
+                            .map(([key, value]) => {
+
+                                return  <Product key={value.id}
+                                        title={this.state.produtos[value.id].title}
+                                        description={this.state.produtos[value.id].description}
+                                        nameImg={this.state.produtos[value.id].img.name}
+                                        url={this.state.produtos[value.id].img.url}
+                                        price={this.state.produtos[value.id].price}
                                     />
                             })
                         }                        
@@ -149,8 +175,8 @@ class App extends Component {
                         <Pagination
                         activePage={this.state.activePage}
                         itemsCountPerPage={this.state.totalItems}
-                        totalItemsCount={Object.keys(this.state.produtos).length}
-                        pageRangeDisplayed={6}
+                        totalItemsCount={Object.keys(this.state.pesquisaFiltro).length}
+                        pageRangeDisplayed={4}
                         onChange={this.handlePageChange}
                         />
                     </div> 
